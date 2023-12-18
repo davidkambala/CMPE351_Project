ef653ef7249224961828cdd89a3a1664fdd642b0
David <davidktshienda@gmail.com>
2023-12-18 16:00:58 +0200

    Made the skeleton of the project

--- cpe351.cpp
+++ cpe351.cpp
@@ -18,20 +18,65 @@ struct Scheduler {
 
 
 int main(int argc, char* argv[]) {
-    int choice;
+    //THE OUTPUT FILE SHOULD BE HANDLED BEFORE THE MENU IS LOADED
+
+
+    int choice_simulator, method_choice, time_quantum;
     cout<<"CPU Scheduler Simulator\n";
-    cout<<"1) Scheduling Method (None by Default)"<<endl;
-    cout<<"2) Preemptive Mode (Off)"<<endl;
+    cout<<"1) Set Scheduling Method"<<endl;
+    cout<<"2) Set Preemptive Mode"<<endl;
     cout<<"3) Show Result"<<endl;
     cout<<"4) End Program"<<endl;
-    cin>>choice;
-    while(choice != 4){
+    cout<<"Option > ";
+    cin>>choice_simulator;
+    while(choice_simulator != 4){
+        //START OF THE SIMULATION
+        switch(choice_simulator){
+            case 1:
+                cout<<"Choose scheduling method, none by default: "<<endl; //current is!!!
+                cout<<"1) First Come, First Served Scheduling"<<endl;
+                cout<<"2) Shortest-Job-First Scheduling"<<endl;
+                cout<<"3) Priority Scheduling"<<endl;
+                cout<<"4) Round-Robin Scheduling"<<endl; //case 4 ask time quantum
+                cout<<"Option > ";
+                cin>>method_choice;
+                switch(method_choice){
+                    case 1:
+                        //FCFS
+                        break;
+                    case 2:
+                        //SJFS
+                        break;
+                    case 3:
+                        //PRIORITY
+                        break;
+                    case 4:
+                        //RR
+                        break;
+                    default:
+                        cout<<"INVALID CHOICE"<<endl;
+                        break;
+                }
+                break;
+            case 2:
+                //PREEMPTIVE MODE
+                break;
+            case 3:
+                //SHOW RESULT
+                break;
+            default:
+                cout<<"INVALID OPTION"<<endl;
+                break;
+        }
+
+
         cout<<"CPU Scheduler Simulator\n";
-        cout<<"1) Scheduling Method (None by Default)"<<endl;
-        cout<<"2) Preemptive Mode (Off)"<<endl;
+        cout<<"1) Set Scheduling Method"<<endl;
+        cout<<"2) Set Preemptive Mode"<<endl;
         cout<<"3) Show Result"<<endl;
         cout<<"4) End Program"<<endl;
-
+        cout<<"Option > ";
+        cin>>choice_simulator;
     }
     return 0;
 }
